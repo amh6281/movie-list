df205c7e86ca80f2037392d0b36257531cdd839e
amh6281 <amh6281@naver.com>
2021-11-29 10:09:28 +0900

    Feat: 영화 id를 가져와 detail 컴포넌트에 추가 2021.11.28

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,29 @@
-function Detail() {
-  return <h1>Detail</h1>;
-}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+const Detail = () => {
+  const [movie, setMovie] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const getDetail = async () => {
+    const detail = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(detail.data.movie);
+    console.log(movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        "Loading...."
+      ) : (
+        <img src={movie.medium_cover_image} alt={movie.id}></img>
+      )}
+    </div>
+  );
+};
 export default Detail;
