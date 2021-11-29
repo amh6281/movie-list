9a15107c7353329efb7f72f6c78fb5b5043f44d8
amh6281 <amh6281@naver.com>
2021-11-29 14:12:52 +0900

    Feat: 배경 색상 추가 2021.11.28

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
 const Detail = () => {
   const [movie, setMovie] = useState("");
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
+
   const getDetail = async () => {
     const detail = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
